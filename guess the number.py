c6f9d97b9abf2504f7d4d28606fe3dc68557b3fa
mamawerecat <93558581+mamawerecat@users.noreply.github.com>
2021-11-02 23:02:40 -0400

    Guess the Number Game
    Simple Guess the Number game. Number is randomly generated by the program.

---
+++ guess the number.py
@@ -0,0 +1,16 @@
+import random
+numbers = range(1, 21)
+random_num = random.choice(numbers)
+guess = int(input("Guess a number 1-20"))
+while guess != random_num:
+    if guess > random_num:
+        print("Lower")
+        guess = int(input("Try again. Guess a number 1-20"))
+    elif guess < random_num:
+        print("Higher")
+        guess = int(input("Try again. Guess a number 1-20"))
+if guess == random_num:
+    print("Winner!!!")
+
+
+##By Mamawerecat
