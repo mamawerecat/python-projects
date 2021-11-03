57d29811cd55c2c4421accb3aa557bc561efbe09
mamawerecat <93558581+mamawerecat@users.noreply.github.com>
2021-11-02 23:25:11 -0400

    Update guess the number.py

--- guess the number.py
+++ guess the number.py
@@ -1,16 +1,16 @@
 import random
-numbers = range(1, 21)
+numbers = range(1, 51)
 random_num = random.choice(numbers)
-guess = int(input("Guess a number 1-20"))
+guess = int(input("Guess a number 1-50"))
 while guess != random_num:
     if guess > random_num:
         print("Lower")
-        guess = int(input("Try again. Guess a lower number 1-20"))
+        guess = int(input("Try again."))
     elif guess < random_num:
         print("Higher")
-        guess = int(input("Try again. Guess a higher number 1-20"))
+        guess = int(input("Try again."))
 if guess == random_num:
-    print("Winner!!!")
+    print("*~*~*~*Winner!!!*~*~*~*")
 
 
 ##By Mamawerecat
